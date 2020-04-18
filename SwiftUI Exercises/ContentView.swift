fb422c8a1af72d1827deee6a75dd5a32437881a1
cod3breaker <ravisherath@gmail.com>
2020-04-18 20:13:04 +1000

    Assigning a fixed size to a view

--- SwiftUI Exercises/ContentView.swift
+++ SwiftUI Exercises/ContentView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        FixedSizeView()
+    }
+}
+
+struct FixedSizeView: View {
+    var body: some View {
+        Text("Hello, SwiftUI")
+            .frame(width: 250, height: 100, alignment: .leading)
+            .background(Color.yellow)
     }
 }
 
