837dfa631c805a7c5e2db999b026d6b6e8fd8b51
cod3breaker <ravisherath@gmail.com>
2020-04-18 19:40:44 +1000

    Initial Commit

---
+++ SwiftUI Exercises/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Exercises
+//
+//  Created by Sunimal Herath on 18/4/20.
+//  Copyright © 2020 Sunimal Herath. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
