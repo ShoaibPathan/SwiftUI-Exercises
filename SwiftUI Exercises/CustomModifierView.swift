52fa3b40d7cfc14ced37f0e0a857989bb1105b71
cod3breaker <ravisherath@gmail.com>
2020-04-20 19:59:07 +1000

    Using custom modifiers on image view

---
+++ SwiftUI Exercises/CustomModifierView.swift
@@ -0,0 +1,34 @@
+//
+//  CustomModifierView.swift
+//  SwiftUI Exercises
+//
+//  Created by Sunimal Herath on 20/4/20.
+//  Copyright © 2020 Sunimal Herath. All rights reserved.
+//
+
+import SwiftUI
+
+struct CustomModifierView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "envelope.circle")
+                .modifier(CustomModifier())
+            Image(systemName: "arkit")
+                .modifier(CustomModifier())
+        }
+    }
+}
+
+struct CustomModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(Font.system(size: 60))
+            .foregroundColor(Color.red)
+    }
+}
+
+struct CustomModifierView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomModifierView()
+    }
+}
